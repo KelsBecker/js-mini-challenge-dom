f0a5c2fd15bc52feb1bb42c9f1101caf5462d7c4
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-03-12 17:28:40 -0400

    fourth deliverable complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /***** Deliverable 1 *****/
 const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+// console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
@@ -23,9 +23,11 @@ PLAYERS.forEach(function(player){
    
 
 
-console.log("PLAYERS array looks like this:", PLAYERS)
+// console.log("PLAYERS array looks like this:", PLAYERS)
 
 
 
 
 /***** Deliverable 4 *****/
+
+document.querySelector(`[data-number="7"]`).remove()
